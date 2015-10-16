59596eed9c55af84d5972d18e076af90ea192c96
lucianoch <lucianoch@users.noreply.github.com>
2015-10-16 02:42:13 -0300

    Semi-finished, without errors, still manual parameters

--- ann.js
+++ ann.js
@@ -6,10 +6,11 @@ function repeat(x, n){
 	return out;
 }
 
-function GAMMA(){ return 0.5; } // weight decay parameter
+function GAMMA(){ return 0.00001; } // weight decay parameter
 function ETA(){ return 0.5; } // learning rate
-function ITER(){ return 100; } // number of iterations
+function ITER(){ return 500; } // number of iterations
 function U(){ return 0.5; } // momentum
+function show_error(){ return true; } // show error on every step of training
 
 
 function new_ann(n1, n2, n3){
@@ -25,11 +26,11 @@ function new_ann(n1, n2, n3){
 		show: function(){
 			return [
 				"n1: " + this.n1 + "; n2: " + this.n2 + "; n3: " + this.n3 + ";",
-				JSON.stringify(this.layer_1),
-				JSON.stringify(this.layer_2),
-				JSON.stringify(this.layer_3),
-				JSON.stringify(this.weights_1),
-				JSON.stringify(this.weights_2)
+				"layer_1:" + JSON.stringify(this.layer_1),
+				"layer_2:" + JSON.stringify(this.layer_2),
+				"layer_3:" + JSON.stringify(this.layer_3),
+				"weights_1:" + JSON.stringify(this.weights_1),
+				"weights_2:" + JSON.stringify(this.weights_2)
 			].join("\n");
 		}
 	};
@@ -81,26 +82,16 @@ function propagate(ann, input){
 	for(var inp = 0; inp < input_length; inp++){
 		// load the input
 		ann.layer_1[0] = -1;
-		for(var i = 1; i <= ann.n1; i++)
-			ann.layer_1[i] = input[i];
+		for(var i = 1; i <= ann.n1; i++) // DIFFERENT INDEX (-1)
+			ann.layer_1[i] = input[i-1];
 		// run the net
 		run_ann(ann);
 		// copy the results
 		for(var i = 1; i <= ann.n3; i++)
-			predictions[i] = ann.layer_3[i];
+			predictions[i-1] = ann.layer_3[i]; // DIFFERENT INDEX (-1)
 	}
 
-	// weight-decay calculation
-	for(var j = 1; j <= ann.n2; j++)
-		for(var k = 0; k <= ann.n1; k++)
-			weight_decay_aux += Math.pow(ann.weights_1[j][k], 2);
-	for(var i = 1; i <= ann.n3; i++)
-		for(var j = 0; j <= ann.n2; j++)
-			weight_decay_aux += Math.pow(ann.weights_2[i][j], 2);
-	weight_decay_aux *= GAMMA();
-	weight_decay_aux /= (ann.n2*(ann.n1+1) + ann.n3*(ann.n2+1));
-
-	return [weight_decay_aux, predictions];
+	return predictions;
 
 }
 
@@ -141,14 +132,15 @@ function train(ann, xs, ys){ // xs and ys must have the same length (xs: list of
 
 			// load input
 			for(var k = 1; k <= ann.n1; k++)
-				ann.layer_1[k] = xs[nu][k];
+				ann.layer_1[k] = xs[nu][k-1];
 
 			// run the net
+			//console.log(ann.show());
 			run_ann(ann);
 
 			// load target
 			for(var k = 1; k <= ann.n3; k++)
-				target[k] = ys[nu][k];
+				target[k] = ys[nu][k-1]; // DIFFERENT INDEX (-1)
 
 			// gradient calculation (layer 3)
 			for(var i = 1; i <= ann.n3; i++)
@@ -179,6 +171,30 @@ function train(ann, xs, ys){ // xs and ys must have the same length (xs: list of
 				}
 
 		} // ends inner loop
+		
+		if( show_error() ){
+			var error = 0;
+			for(var nu = 0; nu < xs_length; nu++){
+
+				// load input
+				for(var k = 1; k <= ann.n1; k++)
+					ann.layer_1[k] = xs[nu][k-1];
+
+				// run the net
+				run_ann(ann);
+
+				// load target
+				for(var k = 1; k <= ann.n3; k++)
+					target[k] = ys[nu][k-1];
+
+				// gradient calculation (layer 3)
+				for(var i = 1; i <= ann.n3; i++)
+					error += Math.pow(target[i] - ann.layer_3[i], 2);
+
+				console.log(error);
+
+			}
+		}
 
 	} // ends main loop
 
@@ -186,10 +202,8 @@ function train(ann, xs, ys){ // xs and ys must have the same length (xs: list of
 
 }
 
-//alert( JSON.stringify( parse_data("1,2 ,3;   4,  5,6 ; 7,8,   9", ";", ',') ) );
 
-
-
+// test the algorithm, function: f(x) = x + 1 (binary!)
 
 var the_net = new_ann(3, 6, 3);
 
@@ -202,14 +216,24 @@ function nat2bin(n){ // from 0 to 7
 	return [p2, p1, p0];
 }
 
-xs = [nat2bin(0), nat2bin(3), nat2bin(4), nat2bin(6)];
-ys = [nat2bin(1), nat2bin(4), nat2bin(5), nat2bin(7)];
+function bin2nat(xs){ // from 0 to 7
+	if(xs[0] < 0.5) xs[0] = 0; else xs[0] = 1;
+	if(xs[1] < 0.5) xs[1] = 0; else xs[1] = 1;
+	if(xs[2] < 0.5) xs[2] = 0; else xs[2] = 1;
+	return xs[0] * 4 + xs[1] * 2 + xs[2] * 1;
+}
 
+train_list = [0, 3, 4, 6];
+
+xs = train_list.map(nat2bin);
+ys = train_list.map( function(x){ return nat2bin(x + 1); } );
 
 train(the_net, xs, ys);
 
+console.log("Trained (difference, ideal: 1):");
+for(var i = 0; i < train_list.length; i++)
+	console.log( [bin2nat(propagate(the_net, nat2bin(train_list[i]))) - train_list[i]] );
 
-results = propagate(the_net, nat2bin());
-
-
-alert(results);
+console.log("All (difference, ideal: 1):");
+for(var i = 0; i < 8; i++)
+	console.log( [bin2nat(propagate(the_net, nat2bin(i))) - i] );
